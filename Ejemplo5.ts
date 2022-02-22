4ec1c7531cc6ecf71ce12ae7eaef479ab97d91fb
Rodolfo <rqm@outlook.es>
2022-02-22 14:00:59 -0600

    Array

---
+++ Ejemplo5.ts
@@ -0,0 +1,9 @@
+let numeros=[1,4,5,7]
+console.log(numeros);
+
+let cadena=['Hola', 'soy', 'Rodolfo'];
+console.log(cadena);
+
+let cadena2:string[]=['Hola', 'soy', 'Rodolfo'];
+console.log(cadena2);
+
