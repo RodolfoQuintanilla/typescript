ab9122f73ec88fe2a6108f2a6e464932d0ce7cf1
Rodolfo <rqm@outlook.es>
2022-02-23 20:08:24 -0600

    funciones 3

---
+++ Funciones.ts
@@ -0,0 +1,9 @@
+function sumar(x:number, y:number) {
+    return (x+y)
+}
+console.log(sumar(3,5));
+
+const sinNada=(r:string)=>{
+console.log(r);
+}
+sinNada('Roro')
---
+++ Funciones2.ts
@@ -0,0 +1,5 @@
+let sumarAnonimo= (x:number, y:number):number=>{
+    return x+y
+}
+
+sumarAnonimo(15,20)
